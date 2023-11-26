0b800eb9f80a63a59b55fe7b2d0e2f9eebd8b8e1
Késia Silva <kesiasilvab6@gmail.com>
2023-11-26 18:02:11 -0300

    Create exemplo2-sem_if_else.c

---
+++ exemplo2-sem_if_else.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/*crie um codigo que solicite ao usuario 6 notas
+e depois apresente a soma dos numeros par e a soma dos 
+numeros impares, e por fim a media das notas;*/
+
+int main(){
+	int i, nota[6], par = 0, impar = 0;
+	float soma = 0, media = 0;
+	
+	for(i=1; i<=6; i++){
+		printf("digite a nota %i\n", i);
+		scanf("%i", &nota[i]);
+		soma+= nota[i];
+	}
+	
+	printf("notas digitadas: ");
+	for(i=1; i<=6; i++){
+		printf("%i ", nota[i]);
+	}
+	
+	for(i=0; i < soma; i+=2){
+		par+= i;
+	}
+	
+	for(i=1; i < soma; i+=2){
+		impar+= i;
+	}
+	
+	media = soma / 6;
+	
+	printf("\nsoma dos numeros pares: %d\n", par);
+	printf("soma dos numeros impares: %d\n", impar);
+	printf("media = %.2f", media);
+	
+}
